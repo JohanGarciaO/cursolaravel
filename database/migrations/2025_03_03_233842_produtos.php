3c0d503801e93d7596c8bf7033b00157708a2e2b
3S JOHAN <johangarciadeoliveira12381@outlook.com.br>
2025-03-03 20:54:36 -0300

    migrations, models and table structure: foreign key

---
+++ database/migrations/2025_03_03_233842_produtos.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Produtos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id(); // Cria um campo id (autoincrement, inteiro e primary key)
+            $table->string('nome'); // campo nome do tipo string
+            $table->text('descricao'); // campo descricao do tipo text
+            $table->double('preco', 10, 2); // campo preco do tipo double com 2 casas decimais
+            $table->string('slug'); // campo slub do tipo string
+            $table->string('imagem'); // campo imagem para armazenar o path de uma imagem
+
+            $table->unsignedBigInteger('id_user'); // campo que vai armazenar o valor da FK usuário
+            $table->foreign('id_user')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade'); // Cria o relacionamento da FK dizendo onde tá referenciando e qual tipo de relacionamento, neste caso 'cascade'
+            
+            $table->unsignedBigInteger('id_categoria'); // campo que vai armazenar o valor da FK categoria
+            $table->foreign('id_categoria')->references('id')->on('categorias')->onDelete('cascade')->OnUpdate('cascade');
+
+            $table->timestamps(); // cria dois campos: created_at e updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
