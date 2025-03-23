ccae3fd070d10ce1a2fb8cc056aef84e25789fe4
Johan Garcia <johangarciadeoliveira123@outlook.com.br>
2025-03-23 17:01:06 -0300

    MiddleWares

--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,4 @@ Route::post('/auth', [LoginController::class, 'auth'])->name('login.auth');
 Route::view('/login', 'login.form')->name('login.form');
 Route::get('/logout', [LoginController::class, 'logout'])->name('login.logout');
 
-Route::get('/admin/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+Route::get('/admin/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard')->middleware(['auth','checkemail']);
